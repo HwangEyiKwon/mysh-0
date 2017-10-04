31e50195f29f15c41291e886822cc483f2819ef4
HwangEyiKwon <hwang21035@naver.com>
2017-10-05 01:53:23 +0900

    utils.c -> require to additional change, commands.c also

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,61 @@
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdio_ext.h>
 
 #include "commands.h"
+
+#define BUFFER_SIZE 255
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
+ // const char* path = argv[1];
+  //strcpy(path, argv[1]);
+ // printf("%s", path);
+ // chdir(argv[1]);
+  chdir("..");
+  printf("cd did\n");
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+  char buffer[BUFFER_SIZE];
+  int buffer_size = 255;
+  getcwd(buffer, buffer_size);
+  printf("%s\n",buffer);
+  //__fpurge((FILE*)buffer);
+  //free(buffer);
+  printf("pwd did\n");
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(argc != 2 || (strcmp(argv[0], "cd")))
+	return 0;
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  printf("%s, %d\n",argv[0], argc);
+  if(argc != 1 || (strcmp(argv[0], "pwd")))
+	return 0;
   return 1;
 }
+/*
+int main() {
+  int a = 1;
+  char* b[2];
+//  b = (char**) calloc(2, sizeof(char*));
+//  b[0] = (char*) calloc(3, sizeof(char));
+//  b[1] = (char*) calloc(3, sizeof(char));
+  
+  b[0] = "cd";
+  b[1] = "..";
+  do_pwd(a, b);
+  do_cd(a, b);
+  do_pwd(a,b);
+}*/
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,60 @@
 #include "utils.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char* command_temp = (char*) command;
+	char* command_token;
+	int command_token_len;
+	int string_num = 1;	
+	char* strptr;
+	(*argc) = 0;
+
+	strptr = strchr(command_temp, ' ');/*
+	//there is only blank ex)"\t" " " "\n"
+	if(!strtok(command_temp, " \t\n")){
+		*argv = (char**)calloc(1, sizeof(char*));
+                *argc = 1;
+                (*argv)[0] = (char*)calloc(1, sizeof(char));
+                (*argv)[0] = NULL;
+		printf("fucking goood\n");
+	}*/
+	//there is only one string ex) pwd
+	if(strptr == NULL){
+		command_token = strtok(command_temp, "\n");
+		command_token_len = strlen(command_token);
+		*argv = (char**)calloc(string_num, sizeof(char*));
+		(*argv)[0] = (char*)calloc((command_token_len + 1), sizeof(char));
+		(*argc) = 1;
+		strcpy((*argv)[0], command_temp);	
+	}
+	else{
+		while(strptr != NULL){
+			string_num++;
+			strptr = strchr(strptr+1, ' ');
+		}
+		command_token = strtok(command_temp, " \t\n");
+		*argv = (char**)calloc(string_num, sizeof(char*));
+		for(int i = 0; command_token != NULL; i++){
+			command_token_len = strlen(command_token);
+			(*argc)++;
+			(*argv)[i] = (char*)calloc((command_token_len + 1), sizeof(char));
+			strcpy((*argv)[i], command_token);
+			command_token = strtok(NULL, " \t\n");	
+			printf("%s, The %dth.\n", (*argv)[i], *argc);
+		}
+	}
+	printf("SUCCESS!\n");
 }
+/*
+int main(){
+	const char str[] = "cd test hwang";
+	int a;
+	char** b;
+
+	mysh_parse_command(str, &a, &b);
+	return 0;
+}*/
