904948588867fe4db9f7ce0c5c54b61b4d7e9aff
HwangEyiKwon <hwang21035@naver.com>
2017-10-09 00:43:29 +0900

    Final Version, 171009

--- src/commands.c
+++ src/commands.c
@@ -14,10 +14,12 @@ int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
   const char* path;
+  //if argv[1] is "~" -> change path to home directory, because "~" is not realized
   if(!strcmp(argv[1], "~"))
 	path = "/home/aeis";
   else
 	path = argv[1];
+  //Unix system call chdir: change working directory
   chdir(path);
   return 0;
 }
@@ -27,6 +29,7 @@ int do_pwd(int argc, char** argv) {
     return -1;
   char buffer[BUFFER_SIZE];
   int buffer_size = 255;
+  //Unix system call getcwd: get current directory
   getcwd(buffer, buffer_size);
   printf("%s\n",buffer);
   return 0;
@@ -41,7 +44,8 @@ int validate_cd_argv(int argc, char** argv) {
   if(argc != 2 || (strcmp(argv[0], "cd")))
 	return 0;
   directory_name = argv[1];
-  //check if 'directory_name' dosen't exist
+  //check if 'directory_name' dosen't exist, "~" is exception
+  //Unix system call stat: get file status
   if((return_stat = stat(directory_name, &directory_info)) == -1 && strcmp(directory_name, "~")){
 	printf("%s: not exist such directory\n",directory_name);
 	return 0;
@@ -56,10 +60,12 @@ int validate_cd_argv(int argc, char** argv) {
 }
 
 int validate_pwd_argv(int argc, char** argv) {
+  //check if command is "pwd" or not
   if(argc != 1 || (strcmp(argv[0], "pwd")))
 	return 0;
   return 1;
 }
+
 
 
 
